c1f0aff54b35a2983c6f2dd51abe0ab54e8c8104
shikutsn <46595890+shikutsn@users.noreply.github.com>
2019-03-04 07:45:57 +0300

    начало

---
+++ js/scripts.js
@@ -0,0 +1,2 @@
+var featuresSliderWindow = document.querySelector(".promo-features-slider-container");
+var featuresSliderNextButton = featuresSliderWindow.querySelector(".promo-features-slider-container::after");
